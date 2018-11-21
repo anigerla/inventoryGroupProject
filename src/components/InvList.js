3d17165c2d2fb1b99a6928f89cda2908606c4570
Regina <regina.zaripova@gmail.com>
2018-11-21 14:17:51 -0500

    initial commit of Inventory List file

---
+++ src/components/InvList.js
@@ -0,0 +1,24 @@
+//the UI for displaying the list of inventory items
+import React, { Component } from 'react';
+
+export default class InvList extends Component {
+    render() {
+        return (
+
+        )
+    }
+}
+
+
+
+// This component will need state.
+// state setting for Inventory List data - will be moved to the main parent component
+// state = {
+//     invList: data,
+// }
+
+// Be sure to create an individual item component.
+
+// This component will also be used to display the inventory for a single warehouse.Your fetch to the API will differ depending on if there is a warehouse_id prop or not. Be sure to collaborate with the person who has the ticket “Front - End: Warehouse Inventory List“.
+
+// Note: If the inventory endpoint on the back - end is not complete, use fake data until it is ready to be hooked up.
