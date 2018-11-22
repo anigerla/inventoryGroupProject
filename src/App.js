623104694c76ac9cb2d263233a16ecf8a2d41918
Edward W <edward.i.wu@gmail.com>
2018-11-22 11:25:47 -0500

    commiting changes to merge

--- src/App.js
+++ src/App.js
@@ -3,19 +3,27 @@ import logo from './logo.svg';
 import './styles/App.css';
 import './styles/index.css';
 import {BrowserRouter as Router, Router, Link, Switch} from 'react-router-dom';
+import NavBar from './components/NavBar.js';
+import SideBar from './components/SideBar.js';
+import InventoryDetails from './components/InvetoryDetails.js'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Router>
-          <Switch>
-            <Route path='/warehouses'/>
-            <Route path='/inventory'/>
-            <Route path='/warehouses/:id'/>
-            <Route path='/inventory/:id'/>
-            <Route path='/'/>
-          </Switch>
+          <div>
+            <NavBar/>
+            <SideBar/> 
+            <Switch> 
+              <Route path='/warehouses'/>
+              <Route path='/inventory'/>
+              <Route path='/warehouses/:id'/>
+              <Route path='/inventory/:id' component={InventoryDetails}/>
+              <Route path='/'/>
+            </Switch>
+          </div>
         </Router>
       </div>
     );
