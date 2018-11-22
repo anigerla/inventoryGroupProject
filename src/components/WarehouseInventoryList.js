b0193f9e098220a67f93866d98ce5b3f51dbd176
Edward W <edward.i.wu@gmail.com>
2018-11-22 09:54:44 -0500

    keeping changes for merge

---
+++ src/components/WarehouseInventoryList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+
+export default class WarehouseInventoryList extends Component {
+  render() {
+    return (
+      <div>
+        <button>example</button>
+        <input placeholder='Search here '/>
+      </div>
+    )
+  }
+}
