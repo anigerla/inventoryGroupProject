7878f6fb0508ae3704980bde7435a8cb7e670bc2
Edward W <edward.i.wu@gmail.com>
2018-11-23 10:41:07 -0500

    routes commited so not change

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
 import './styles/App.css';
 import './styles/index.css';
-import {BrowserRouter as Router, Router, Link, Switch} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
 import NavBar from './components/NavBar.js';
-import SideBar from './components/SideBar.js';
-import InventoryDetails from './components/InvetoryDetails.js'
+import InventoryDetails from './components/InventoryDetails.js'
 
 import SideBar from './components/SideBar';
 import MainComponent from './components/MainComponent';
@@ -14,15 +13,14 @@ class App extends Component {
     return (
       <div className="App">
         <Router>
-          <div>
+          <div className="container">
             <SideBar/>
-            <MainComponent /> 
             <Switch> 
               <Route path='/warehouses'/>
               <Route path='/inventory'/>
               <Route path='/warehouses/:id'/>
               <Route path='/inventory/:id' component={InventoryDetails}/>
-              <Route path='/'/>
+              <Route path='/' component={MainComponent}/>
             </Switch>
           </div>
         </Router>
--- src/components/WarehouseInventoryList.js
+++ src/components/WarehouseInventoryList.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react'
 export default class WarehouseInventoryList extends Component {
   render() {
     return (
-      <div>
+      <div className='main'>
         <button>example</button>
         <input placeholder='Search here '/>
       </div>
