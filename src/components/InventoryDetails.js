3c0887e25b7b8e68ef8924f2da74d8b5a496fe52
Edward Wu <edward.i.wu@gmail.com>
2018-11-25 16:27:16 -0500

    commit to merge with working branch

--- src/components/InventoryDetails.js
+++ src/components/InventoryDetails.js
@@ -2,9 +2,31 @@ import React, { Component } from 'react'
 
 export default class InventoryDetails extends Component {
   render() {
+
+   let item = {
+      "id": "B4",
+      "productName": "Uranium Ore",
+      "productdescr": "It's nuclear",
+      "lastOrder": "05/24/2018",
+      "location": "Thunder Bay",
+      "quantity": "0.5",
+      "status": "In Stock",
+      "warehouseId": "A4",
+      "description":"Here is a more detailed summary of the product name, it’s uses, industries"
+                     +"and possible attributes that could be used to describe the product."
+  }
+
     return (
-      <div>
-        <h1>Product Name Summary</h1>
+      <div className='InvDetailsParent'>
+        <div className='detailCard'>
+          <div className='title'>
+            <h1 className='title__name'>Product Name Summary</h1>
+            <button className='title__newOrderButton'> New Order </button>
+          </div>
+          <div className='details'>
+            {this.props.match.params.id}
+          </div>
+        </div>
       </div>
     )
   }
